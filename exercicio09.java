047f9a7c830f179d2a75d1094a08f714a10a9f7a
viniciusferreira001 <vinicius.assis@ufv.br>
2022-09-26 21:06:13 -0300

    9 consertada

--- exercicio09.java
+++ exercicio09.java
@@ -6,15 +6,27 @@ public class exercicio09 {
         String entrada;
         Scanner ler = new Scanner(System.in);
 
-        System.out.println("Digite a operação desejada:");
-        entrada = ler.next();
-        
-        String primeira = entrada.substring(0, (entrada + " ").indexOf(" "));
-        int numeroA = Integer.parseInt(entrada.substring(1, (entrada + " ").indexOf(" ")));
-        //if(entrada[0] == "soma"){
+        System.out.println("Digite a operação desejada no seguinte formato:\nMULTIPLICA A POR B;\nDIVIDE A POR B;\nSOMA A POR B;\nSUBTRAI A POR B.");
+        entrada = ler.nextLine();
 
-        //}
-        System.out.printf("%d", numeroA);
+        String[] subEntrada = entrada.split(" ");
+        
+        float numeroA = Float.parseFloat(subEntrada[1]);
+        float numeroB = Float.parseFloat (subEntrada[3]);
+
+        if (subEntrada[0].equals("SOMA")){
+            System.out.printf("A soma de %.2f por %.2f eh: %.2f\n", numeroA, numeroB, numeroA+numeroB);
+        }else if (subEntrada[0].equals("SUBTRAI")){
+            System.out.printf("A subtração de %.2f por %.2f eh: %.2f\n", numeroA, numeroB, numeroA-numeroB);
+        }else if (subEntrada[0].equals("MULTIPLICA")){
+            System.out.printf("A multiplicação de %.2f por %.2f eh: %.2f\n", numeroA, numeroB, numeroA*numeroB);
+        }else if (subEntrada[0].equals("DIVIDE")){
+            System.out.printf("A divisão de %.2f por %.2f eh: %.2f\n", numeroA, numeroB, numeroA/numeroB);
+        }else{
+            System.out.println("Entrada de operação inválida");
+        }
+
+        System.out.printf("SISTEMA ENCERRADO!!!\n");
 
 
         ler.close();
